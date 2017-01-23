d1b1f1e4b7a5af5148140ce2751f6a36ba7aa06a
moulichowdhary <moulichowdhary@gmail.com>
2017-01-23 08:50:13 -0600

    Added complaint file activity and screen

---
+++ app/src/main/java/com/example/s525721/myapplication/ComplaintActivity.java
@@ -0,0 +1,29 @@
+package com.example.s525721.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class ComplaintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint);
+    }
+
+    private EditText data = (EditText) findViewById(R.id.complaintData);
+
+
+    protected boolean fileComplaint(){
+
+return true;
+
+
+
+    }
+
+
+
+
+}
